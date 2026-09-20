228603925544611cac881476aa3e6c994382c37f
Suden <20457922+shudnal@users.noreply.github.com>
2026-09-21 00:51:03 +0300

    Add unknown requirement icon color config

--- CircletExtended.cs
+++ CircletExtended.cs
@@ -77,6 +77,7 @@ namespace CircletExtended
         public static ConfigEntry<string> circletRecipeQuality2;
         public static ConfigEntry<string> circletRecipeQuality3;
         public static ConfigEntry<string> circletRecipeQuality4;
+        public static ConfigEntry<Color> circletUnknownRequirementIconColor;
 
         public static ConfigEntry<string> circletHelmetWhiteList;
         public static ConfigEntry<string> circletHelmetBlackList;
@@ -240,6 +241,7 @@ namespace CircletExtended
             circletRecipeQuality2 = config("Circlet - Recipe", "Upgrade quality 2", defaultValue: "Resin:20,LeatherScraps:10,IronNails:10,Chain:1", "Recipe to upgrade circet to quality 2");
             circletRecipeQuality3 = config("Circlet - Recipe", "Upgrade quality 3", defaultValue: "Thunderstone:5,Silver:1,JuteRed:2", "Recipe to upgrade circet to quality 3");
             circletRecipeQuality4 = config("Circlet - Recipe", "Upgrade quality 4", defaultValue: "Demister:1,BlackCore:1", "Recipe to upgrade circet to quality 4");
+            circletUnknownRequirementIconColor = config("Circlet - Recipe", "Unknown requirement icon color", defaultValue: (Color)new Color32(0x21, 0x21, 0x21, 0xB1), "Icon color used for undiscovered upgrade recipe requirements. [Not Synced with Server]", false);
 
             circletRecipeQuality1.SettingChanged += (sender, args) => CircletItem.FillRecipe();
             circletRecipeQuality2.SettingChanged += (sender, args) => CircletItem.FillRecipe();
